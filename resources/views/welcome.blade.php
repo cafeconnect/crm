e1770996bf06f0e26fe8a0eecb156c93e26686e2
cafeconnect <mathewmoroko@gmail.com>
2020-03-04 17:55:38 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Welcome Client</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="index/image/png" href="index/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="index/css/util.css">
+	<link rel="stylesheet" type="text/css" href="index/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+  
+<div class="container-contact100">
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form">
+				<span class="contact100-form-title">
+					Crm Application!
+				</span>
+
+				<div class="wrap-input100"> 
+					<span class="label-input100">Name</span>
+					<input class="input100" type="text" name="name" placeholder="Enter name">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 " >
+					<span class="label-input100">Location</span>
+					<input class="input100" type="text" name="email" placeholder="Enter the location">
+					<span class="focus-input100"></span>
+                </div>
+                
+                <div class="wrap-input100" >
+					<span class="label-input100">Question?</span>
+					<textarea class="input100" name="message" placeholder="Your question here..."></textarea>
+					<span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input100" >
+					<span class="label-input100">Message</span>
+					<textarea class="input100" name="message" placeholder="Your message here..."></textarea>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<div class="wrap-contact100-form-btn">
+						<div class="contact100-form-bgbtn"></div>
+						<button class="contact100-form-btn">
+							<span>
+								Submit
+								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+							</span>
+						</button>
+					</div>
+                </div>
+                <div class="container-contact100-form-btn">
+					<div class="wrap-contact100-form-btn">
+						<div class="contact100-form-bgbtn"></div>
+						<button class="contact100-form-btn">
+							<span>
+								generate report
+								
+							</span>
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+
+	
+
+<!--===============================================================================================-->
+	<script src="index/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="index/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="index/vendor/bootstrap/js/popper.js"></script>
+	<script src="index/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+
+	<script src="index/vendor/daterangepicker/moment.min.js"></script>
+	<script src="index/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="index/vendor/countdowntime/countdowntime.js"></script>
+
+	<script src="index/js/main.js"></script>
+
+
+</body>
+</html>
